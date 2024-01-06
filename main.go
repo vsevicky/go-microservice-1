8eea5aab1a9b28ff59566f6cd70b4d4a363d478e
Vicky Kumar <99803613+vsevicky@users.noreply.github.com>
2024-01-06 19:51:22 +0530

    ep3

--- main.go
+++ main.go
@@ -18,10 +18,12 @@ func main() {
 
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
+	sm.Handle("/products", ph)
 
 	// create a new server
 	s := http.Server{
